b33ed41f814bdd31ccada3733f09de0f6e2f8d75
Andriprayoga <andri7451@smk.belajar.id>
2024-09-04 17:37:33 -0800

    Add files via upload

---
+++ from login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>from login</title>
+	<link rel="stylesheet" type="text/css" href="ujicoba2.html">
+</head>
+<body>
+	<div class="wrapper">
+		<meta name="viewport" content="width-device-width,initial-scale=1">
+		<title>from login</title>
+		<link rel="stylesheet" type="text/css" href="ujicoba2.html">
+<head>
+	<body>
+		<div class="wrapper">
+			<span class="icon-close">
+				<a href="ujicoba-html"><i class="fa-solid fa-xmark"></i></a>
+			</span>
+
+			<form action="form login.html"method="post">
+				<h1>Daftar</h1>
+				<div>
+			
+		</div>
+	</body>
+</head>		
+	</div>
+
+</body>
+</html>
